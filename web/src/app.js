83ffa7b303610f4498a55c9ed67e8ccc2b8e4943
Bartha Levente <b.levi.levi@gmail.com>
2025-04-09 12:10:30 +0200

    Teljesen kész

--- web/src/app.js
+++ web/src/app.js
@@ -1,21 +1,39 @@
+
+
 const tbody = document.querySelector("#tbody")
 //Read
 const url = 'http://localhost:8000/api/employees'
 
+
 //Promise
+function getEmployees(){
+    fetch(url)
+    .then((response)=>{
+        return response.json()
+    })
+    .then((result)=>{
+        console.log(result)
+        empList = result
+        renderTbody(result.data)
+
+    })
+}
+
+getEmployees()
+
+const saveButton = document.querySelector("#saveButton")
+
+const idInput = document.querySelector("#id")
+const nameInput = document.querySelector("#name")
+const cityInput = document.querySelector("#city")
+const salaryInput = document.querySelector("#salary")
 
 
 
-fetch(url)
-.then((response)=>{
-    return response.json()
-})
-.then((result)=>{
-    console.log(result)
-    empList = result
-    renderTbody(result.data)
 
-})
+
+
+
 
 
 
@@ -35,4 +53,54 @@ function renderTbody(empList){
 
     })
     tbody.innerHTML = tboyContent
+}
+
+//create művelet
+
+saveButton.addEventListener("click", () =>{
+    // console.log("mükszik")
+    // console.log(nameInput.value)
+
+    //javascript objektum
+    const emp = {
+        name : nameInput.value,
+        city : cityInput.value,
+        salary : salaryInput.value
+    }
+
+    // console.log(emp)
+
+    addEmployee(emp)
+
+
+    clearFields()
+    
+
+
+    
+
+})
+
+function clearFields(){
+    idInput.value = ""
+    nameInput.value = ""
+    cityInput.value = ""
+    salaryInput.value = ""
+}
+
+function addEmployee(emp){
+    fetch(url, 
+        {method : "post",
+            body : JSON.stringify(emp),
+            headers : {
+                "Content-Type" : "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(result => {
+            getEmployees()
+            console.log(result)
+        })
+        .catch(err => console.log(err))
+
 }
